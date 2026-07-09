96624356bf20fc0c90caa5fc6706c975c90708e2
Daniel Damasio <damasio.dan7@gmail.com>
2026-07-09 09:48:51 -0300

    task: setup ReactNavigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: {
+      screen: Home,
+    },
+  }
+});
+
+const Navigation = createStaticNavigation(RootStack);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Home.tsx
@@ -0,0 +1,20 @@
+import {useNavigation} from '@react-navigation/native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+
+export default function Home() {
+    const navigation = useNavigation();
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up Home.tsx to start working on your app!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+    }
+});
